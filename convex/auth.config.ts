de6d26f221652d65b32eaa342fad3209866d998a
Alia Jennifer <aliyajenny14@gmail.com>
2024-10-16 21:02:56 +0700

    adding files.ts

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://still-caiman-0.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { SignInButton, SignedIn, SignOutButton, SignedOut } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+
+export default function Home() {
+  const createFile = useMutation(api.files.createFile);
+
+  return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <SignedIn>
+          <SignOutButton>
+            <Button>Sign Out</Button>
+          </SignOutButton>
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+
+        <Button
+          onClick={() => {
+            createFile({
+              name: "Hello World",
+            });
+          }}
+        >
+          Click Me
+        </Button>
+      </main>
+  );
+}
